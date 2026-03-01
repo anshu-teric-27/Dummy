37e55a197edc7e4a7d0db563377927d1ec5f19a6
Anshu <anshu@tericsoft.com>
2026-03-02 00:34:56 +0530

    test

--- app/main.py
+++ app/main.py
@@ -5,6 +5,13 @@ from app.api.v1.routes_demo import router as demo_router
 
 def create_app() -> FastAPI:
     app = FastAPI(title="Python Demo API", version="0.1.0")
+    app = FastAPI(
+    title="Python Demo API",
+    version="0.1.0",
+    docs_url=None,
+    redoc_url=None,
+    openapi_url=None,
+)
 
     app.include_router(demo_router, prefix="/api/v1", tags=["demo"])
 
